5d2c8d4bebd00296efbe8652daf66373f138b9b2
Gerrit <gerrit@agri-vision.nl>
2014-01-25 17:10:27 +0100

    added callsign to image

--- sstvcam.sh
+++ sstvcam.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # sstv camera, Gerrit Polder, PA3BYA
+# shell script to test SSTV camera
 raspistill -t 1 --width 320 --height 256 -e png -o /tmp/image.png
-# add callsign, something like
-mogrify /tmp/image.png -pointsize 48 -draw "text 10,250 'call'"
+# add callsign
+mogrify -pointsize 24 -draw "text 10,40 'MYCALL'" /tmp/image.png
 ./pisstv /tmp/image.png 22050
 sudo ./pifm_sstv /tmp/image.png.wav
 rm /tmp/image.png*
